55271e1d522cd8e52d024ea4d3d8235f42c3edac
enriquezcarolina <carosol333@gmail.com>
2022-04-05 09:22:25 -0300

    agregue fin del programa

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(void)
 {
 
-    cout << "hola" << endl;
+  
     cout << "=======================INICIO DEL PROGRAMA===================================" << endl;
     fd = fopen(“textito.txt”, “r”);
     fread(puf, tam, fd);
     fclose(fd);
-
+    cout << "=======================FIN DEL PROGRAMA===================================" << endl;
 }
