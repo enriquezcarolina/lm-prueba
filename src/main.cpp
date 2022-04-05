80c3edc4a4f3ae9d61e40ac2c0b6b13b3ec951ba
enriquezcarolina <66533842+enriquezcarolina@users.noreply.github.com>
2022-04-05 09:18:46 -0300

    agregue al texto algo de wiki (#3)

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,8 @@ int main(void)
 
     cout << "hola" << endl;
     cout << "=======================INICIO DEL PROGRAMA===================================" << endl;
+    fd = fopen(“textito.txt”, “r”);
+    fread(puf, tam, fd);
+    fclose(fd);
+
 }
